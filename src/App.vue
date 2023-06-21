ba64f5340a63c006b7c9205452d9f5c8b6a0fb77
asiapicasso <asia.picasso@gmail.com>
2023-06-21 14:09:21 +0200

    exo working. correction ok.

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,49 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  import TheHeader from './components/TheHeader.vue';
+  import BookmarkEntry from './components/BookmarkEntry.vue';
+  import BookmarkForm from './components/BookmarkForm.vue';
+   import { bookmarks } from './stores/bookmarks.js';
+   import { computed, ref } from 'vue';
+  const showForm = ref(false);
+  // console.log(bookmarks.value);
+  // addBookmark({
+  //   url: 'https://vuejs.org/',
+  //   tags: 'javascript framework',
+  //   label: 'Vue.js',
+  // });
+  // const entry2 = {
+  //   label: 'Ex',
+  //   url: 'https://example.com/',
+  //   tags: 'example',
+  // };
+  // addBookmark(entry2);
+  // console.log(JSON.stringify(bookmarks.value));
+  // remove(entry2);
+  // console.log(bookmarks.value);
+  const filterBy = ref('');
+  const filteredBookmarks = computed(() => bookmarks.value.filter(bookmark => {
+      return bookmark.tags.toLowerCase().includes(filterBy.value.toLowerCase());
+  }));
+  const orderedBookmarks = computed(() => filteredBookmarks.value.sort((a, b) => {
+    return a.label.localeCompare(b.label);
+  }));
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <the-header>Bookmarks</the-header>
+  <div>
+    <label>Filter by: </label>
+    <input type="text" v-model="filterBy" autofocus/>
+  </div>
+  <button @click="showForm = true">Add</button>
+  <bookmark-form
+    v-if="showForm"
+    @close="showForm = false"
+  />
+  <bookmark-entry
+    v-for="bookmark of orderedBookmarks"
+    :bookmark="bookmark"
+  />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
 
-  <main>
-    <TheWelcome />
-  </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,24 @@
+<script setup>
+// import { onMounted, onUnmounted } from 'vue'
+// const x = 7;
+// function update(event) {
+//   console.log('update', event.pageX, event.pageY)
+// }
+// onMounted(() => window.addEventListener('mousemove', update))
+// onUnmounted(() => window.removeEventListener('mousemove', update))
+</script>
+
+<template>
+  <header>
+    <slot></slot>
+  </header>
+</template>
+
+<style scoped>
+  header {
+    background-color: #333;
+    color: #fff;
+    padding: 10px;
+    text-align: center;
+  }
+</style>
